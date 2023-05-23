a1a9f5df6b322147273f3b8414088c3a038cebe0
abuhujair <abuhujair.k@gmail.com>
2023-05-23 16:18:32 +0530

    Tail Call with State

--- examples/my_example/example_tailcall.py
+++ examples/my_example/example_tailcall.py
@@ -25,7 +25,7 @@ int do_tail_call(void *ctx) {
 tail_fn = b.load_func("tail_call", BPF.KPROBE)
 prog_array = b.get_table("prog_array")
 prog_array[c_int(2)] = c_int(tail_fn.fd)
-b.attach_kprobe(event=b.get_syscall_fnname("clone"), fn_name="do_tail_call")
+b.attach_kprobe(event=b.get_syscall_fnname("sync"), fn_name="do_tail_call")
 
 # format output
 while 1:
