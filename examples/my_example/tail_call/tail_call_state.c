09473ad426a9d2074ca97b40952ad03d31f63a78
Abuhujair Khan <abuhujair.k@gmail.com>
2023-05-24 22:58:05 +0530

    Tail Call Program with State Added

--- examples/my_example/tail_call/tail_call_state.c
+++ examples/my_example/tail_call/tail_call_state.c
@@ -1,19 +1,38 @@
+#include <bcc/proto.h>
 #include <net/sock.h>
 
+#define IP_TCP 6
+
 BPF_PROG_ARRAY(prog_array, 10);
-BPF_HASH(tail_state, struct __sk_buff *);
+
+struct tail_call_state{
+    u32 src_ip;
+    u32 dst_ip;
+    u32 src_port;
+    u32 dst_port;
+    u8 tail_call_count;
+};
+
+BPF_HASH(tail_state, struct __sk_buff *, struct tail_call_state);
 
 int tail_call(struct __sk_buff * skb) {
-    u64 *datap = tail_state.lookup(&skb),data;
-    if(datap!=NULL){
-        tail_state.delete(&skb);
-        bpf_trace_printk("Tail-call %d\n", data);
+
+    struct __sk_buff * key = skb;
+    struct tail_call_state * state = tail_state.lookup(&key);
+    if(state!=NULL && state->tail_call_count < 10){
+        bpf_trace_printk("Tail-Call Program %p %u %u\n", key, state->src_port, ++state->tail_call_count);
+        // bpf_trace_printk("Tail-Call Program %u %u %u\n", state->src_ip, state->dst_ip, state->dst_port);
         prog_array.call(skb,2);
+        goto TAIL_EXIT;
+        tail_state.delete(&key);
     }
+TAIL_EXIT:
+    tail_state.delete(&key);
     return 0;
 }
 
 int do_tail_call(struct __sk_buff * skb) {
+    struct __sk_buff * key = skb;
     __u8 *cursor = 0;
     struct ethernet_t *ethernet = cursor_advance(cursor, sizeof(struct ethernet_t));
     struct ip_t *ip = cursor_advance(cursor, sizeof(struct ip_t));
@@ -27,12 +46,18 @@ int do_tail_call(struct __sk_buff * skb) {
     // shift cursor forward for dynamic ip header size
     void *_ = cursor_advance(cursor, (ip_header_length - sizeof(struct ip_t)));
     struct tcp_t *tcp = cursor_advance(cursor, sizeof(struct tcp_t));
-    if (tcp->src_port != PORT_TO_MONITOR)
-        goto EXIT;
 
-    u64 data = 1;
-    tail_state.update(&skb,&data);
-    bpf_trace_printk("Original program %d\n", data);
+    struct tail_call_state state = {};
+    state.src_ip = ip->src;
+    state.dst_ip = ip->dst;
+    state.src_port = tcp->src_port;
+    state.dst_port = tcp->dst_port;
+    state.tail_call_count = 1;
+    
+    tail_state.update(&key,&state);
+    bpf_trace_printk("Original Program %p %u %u\n", key, state.src_port, state.tail_call_count);
+    // bpf_trace_printk("Original Program %u %u %u\n", state.src_ip, state.dst_ip, state.dst_port);
     prog_array.call(skb, 2);
+EXIT:
     return 0;
 }
